33aed85da0579dbe81343c36d963386b659eff60
shikha477 <shikha479saini@gmail.com>
2026-04-09 01:12:52 +0530

    Implemented role-based access middleware

---
+++ Smart-Inventory-System/src/middleware/role.middleware.js
@@ -0,0 +1,20 @@
+const ApiError = require("../utils/ApiError");
+
+const authorizeRoles = (...roles) => {
+
+return (req,res,next)=>{
+
+if(!roles.includes(req.user.role)){
+    throw new ApiError(
+        403,
+        `Access denied. Required role: ${roles.join(", ")}`
+    );
+}
+
+next();
+
+};
+
+};
+
+module.exports = authorizeRoles;
