69e96617341c572a9a6c88f523bf614bd43f6266
shikha477 <shikha479saini@gmail.com>
2026-04-10 00:12:48 +0530

    create supplier model

---
+++ Smart-Inventory-System/src/models/supplier.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const supplierSchema = new mongoose.Schema(
+{
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        lowercase: true
+    },
+
+    phone: {
+        type: String
+    },
+
+    company: {
+        type: String
+    },
+
+    address: {
+        type: String
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("Supplier", supplierSchema);
