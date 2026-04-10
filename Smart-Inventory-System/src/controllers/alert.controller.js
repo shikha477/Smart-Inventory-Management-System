c4a26cc63a2f25cbe93b846335f5279d767af7eb
shikha477 <shikha479saini@gmail.com>
2026-04-10 19:56:22 +0530

    Implement low stock alerts system

---
+++ Smart-Inventory-System/src/controllers/alert.controller.js
@@ -0,0 +1,27 @@
+const Alert = require("../models/alert.model");
+const Product = require("../models/product.model");
+const Inventory = require("../models/inventory.model");
+const asyncHandler = require("../utils/asyncHandler");
+
+
+exports.checkLowStock = async (productId) => {
+
+    const inventory = await Inventory.findOne({ product: productId }).populate("product");
+
+    if (!inventory) return;
+
+    if (inventory.quantity <= inventory.product.lowStockThreshold) {
+
+        const existingAlert = await Alert.findOne({
+            product: productId,
+            isRead: false
+        });
+
+        if (!existingAlert) {
+            await Alert.create({
+                product: productId,
+                message: `Low stock for product: ${inventory.product.name}`
+            });
+        }
+    }
+};
