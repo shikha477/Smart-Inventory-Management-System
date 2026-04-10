4b19d046c575f4e4d0f0a7a8be928b79964af6d6
shikha477 <shikha479saini@gmail.com>
2026-04-10 16:24:47 +0530

    implement create bill API

---
+++ Smart-Inventory-System/src/controllers/billing.controller.js
@@ -0,0 +1,55 @@
+const Bill = require("../models/bill.model");
+const Product = require("../models/product.model");
+const asyncHandler = require("../utils/asyncHandler");
+const ApiError = require("../utils/ApiError");
+
+exports.createBill = asyncHandler(async (req, res) => {
+  const { items } = req.body;
+
+  if (!items || items.length === 0) {
+    throw new ApiError(400, "Bill must contain items");
+  }
+
+  let totalAmount = 0;
+  const billItems = [];
+
+  for (const item of items) {
+    const product = await Product.findById(item.product);
+
+    if (!product) {
+      throw new ApiError(404, "Product not found");
+    }
+
+    if (product.stock < item.quantity) {
+      throw new ApiError(400, `Not enough stock for ${product.name}`);
+    }
+
+    const subtotal = product.price * item.quantity;
+
+    billItems.push({
+      product: product._id,
+      quantity: item.quantity,
+      price: product.price,
+      subtotal,
+    });
+
+    totalAmount += subtotal;
+
+    // Auto stock deduction
+    product.stock -= item.quantity;
+    await product.save();
+  }
+
+  const bill = await Bill.create({
+    billNumber: `BILL-${Date.now()}`,
+    items: billItems,
+    totalAmount,
+    createdBy: req.user?.id,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Bill created successfully",
+    data: bill,
+  });
+});
